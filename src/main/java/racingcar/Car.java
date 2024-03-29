1fdfa29b3df767c31c54726daf18cfda248879a6
krSeonghyeon <ya41921174@gmail.com>
2024-03-29 19:53:37 +0900

    refactor: 코드 컨벤션 (줄바꿈 추가)

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     private final String name;
     private int position;
+
     public Car(String name) {
         this.name = name;
         this.position = 0;
