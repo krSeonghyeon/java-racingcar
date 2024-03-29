b3604f50a7af9f26931200b95ccf1644676a97f3
krSeonghyeon <ya41921174@gmail.com>
2024-03-29 18:49:08 +0900

    feat: 기본 입출력 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        String[] carNameArray = carNames.split(",");
+
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNameArray) {
+            cars.add(new Car(name));
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryCount = Integer.parseInt(Console.readLine());
+
+        System.out.println("실행결과");
+        // 게임 진행 및 결과 출력
     }
 }
