1af04cf486a9f07c48d2e394ebb7e7a92b857f92
krSeonghyeon <ya41921174@gmail.com>
2024-03-29 22:57:11 +0900

    refactor: main에서 game 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,36 +7,7 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        String[] carNameArray = carNames.split(",");
-        validateCarNames(carNameArray);
-
-        List<Car> cars = new ArrayList<>();
-        for (String name : carNameArray) {
-            cars.add(new Car(name));
-        }
-
-        System.out.println("시도할 회수는 몇회인가요?");
-        int tryCount = Integer.parseInt(Console.readLine());
-        validateTryCount(tryCount);
-
-        System.out.println("\n실행결과");
-        Race race = new Race(cars, tryCount);
-        race.startRace();
-    }
-
-    private static void validateCarNames(String[] carNameArray) {
-        for (String carName : carNameArray) {
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
-            }
-        }
-    }
-
-    private static void validateTryCount(int tryCount) {
-        if (tryCount < 1) {
-            throw new IllegalArgumentException("시도 횟수는 1이상이어야 합니다.");
-        }
+        CarRaceGame carRaceGame = new CarRaceGame();
+        carRaceGame.start();
     }
 }
---
+++ src/main/java/racingcar/CarRaceGame.java
@@ -0,0 +1,64 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarRaceGame {
+    public CarRaceGame() {}
+
+    private String[] getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        String[] carNameArray = carNames.split(",");
+        validateCarNames(carNameArray);
+
+        return carNameArray;
+    }
+
+    private void addCar(String[] carNameArray, List<Car> cars) {
+        for (String name : carNameArray) {
+            cars.add(new Car(name));
+        }
+    }
+
+    private int getTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tryCount = Integer.parseInt(Console.readLine());
+        validateTryCount(tryCount);
+
+        return tryCount;
+    }
+
+    private void result(List<Car> cars, int tryCount) {
+        System.out.println("\n실행결과");
+        Race race = new Race(cars, tryCount);
+        race.startRace();
+    }
+
+    public void start() {
+        String[] carNameArray = getCarNames();
+
+        List<Car> cars = new ArrayList<>();
+        addCar(carNameArray, cars);
+
+        int tryCount = getTryCount();
+
+        result(cars, tryCount);
+    }
+
+    private void validateCarNames(String[] carNameArray) {
+        for (String carName : carNameArray) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+            }
+        }
+    }
+
+    private void validateTryCount(int tryCount) {
+        if (tryCount < 1) {
+            throw new IllegalArgumentException("시도 횟수는 1이상이어야 합니다.");
+        }
+    }
+}
