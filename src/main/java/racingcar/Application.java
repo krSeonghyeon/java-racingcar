4b9cde3e896fa0c8a0f35bb75ad43f56dbdb5dfc
krSeonghyeon <ya41921174@gmail.com>
2024-03-29 19:39:40 +0900

    fix: 입출력 줄바꿈 버그 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -21,7 +21,7 @@ public class Application {
         System.out.println("시도할 회수는 몇회인가요?");
         int tryCount = Integer.parseInt(Console.readLine());
 
-        System.out.println("실행결과");
+        System.out.println("\n실행결과");
         Race race = new Race(cars, tryCount);
         race.startRace();
     }
